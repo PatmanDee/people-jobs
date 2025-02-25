817bf4c90bc8d62f8531c4482bf03df3f2c5f0cd
Patson Chidari <patsonchids@gmail.com>
2025-02-25 17:21:58 +0200

    db Job model & factories and db migrations along with factories

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
+
+    protected $guarded = [];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function jobs(): HasMany
+    {
+        return $this->hasMany(Job::class);
+    }
 }
--- database/migrations/2025_02_25_144646_create_employers_table.php
+++ database/migrations/2025_02_25_144646_create_employers_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('employers', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('name');
+            $table->string('logo')->nullable();
             $table->timestamps();
         });
     }
