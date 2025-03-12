8d7fc8a5639ffd03cf28b51e4313ece6ba12c484
Patson Chidari <patsonchids@gmail.com>
2025-03-12 14:39:58 +0200

    creating auth and register controllers

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobController::class, 'index'])->name('home');
 Route::get('/jobs/{job}', [JobController::class, 'show'])->name('jobs.show');
 
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'destroy']);
